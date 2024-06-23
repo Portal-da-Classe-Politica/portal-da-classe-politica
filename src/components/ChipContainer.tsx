fac7dcb3dc5b5ac25a44863f6ddfe60f29cc0754
Chandee <chandemoraes@hotmail.com>
2024-06-23 16:47:39 -0300

    feat: chipContainer component

--- src/components/ChipContainer.tsx
+++ src/components/ChipContainer.tsx
@@ -1,9 +1,28 @@
-const ChipContainer = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <span className="mr-3 bg-orangeLight1 py-[3px] px-[8px] text-c2 text-orange font-bold rounded-[20px] text-nowrap">
-      {children}
-    </span>
-  );
+import { cva } from 'cva';
+
+const chipVariant = cva(
+  'font-montserrat mr-3 text-nowrap text-c2 rounded-[20px] py-[3px] px-[8px] transition-opacity duration-300 hover:opacity-70 cursor-default',
+  {
+    variants: {
+      type: {
+        default: 'bg-orangeLight1 text-orange font-bold',
+        full: 'bg-orange text-white font-bold',
+        ghost: 'text-orange font-bold border border-orange',
+      },
+    },
+    defaultVariants: {
+      type: 'default',
+    },
+  },
+);
+
+interface Props {
+  children: React.ReactNode;
+  type?: 'default' | 'full' | 'ghost';
+}
+
+const ChipContainer = ({ type, children }: Props) => {
+  return <span className={chipVariant({ type })}>{children}</span>;
 };
 
 export default ChipContainer;
