cac09ab187eae73ca5298e318937a3b1125d1153
Chandee <chandemoraes@hotmail.com>
2024-06-25 18:03:24 -0300

    feat: add bullet and fix error on card

---
+++ src/app/storybook/bullet/page.tsx
@@ -0,0 +1,12 @@
+import Bullet from '@/components/bullet/Bullet';
+
+const Test = () => {
+  return (
+    <div>
+      <Bullet name="aa" text="texto 1" />
+      <Bullet name="aa" text="texto 2" />
+    </div>
+  );
+};
+
+export default Test;
---
+++ src/components/bullet/Bullet.tsx
@@ -0,0 +1,17 @@
+import { Text } from '../base';
+
+const Bullet = ({ text, name }: { text: string; name: string }) => {
+  return (
+    <div>
+      <label className="flex items-center cursor-pointer">
+        <input type="radio" className="hidden peer" name={name} />
+        <div className="w-3 h-3 border-[1px] border-orange rounded-full flex items-center justify-center mr-2  peer-checked:bg-orange"></div>
+        <Text size="B2" textType="span" className="text-grayLight3">
+          {text}
+        </Text>
+      </label>
+    </div>
+  );
+};
+
+export default Bullet;
