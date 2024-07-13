f897315a5556005838d093b5f9f427df27acc8d5
Lucas Fabre <lucas.p.fabre@gmail.com>
2024-07-13 14:56:01 +0100

    fix: geoJson layers

--- src/components/map/geojson/index.ts
+++ src/components/map/geojson/index.ts
@@ -1,57 +1,57 @@
-import acre from './acre.json';
-import alagoas from './alagoas.json';
-import amapa from './amapa.json';
-import amazonas from './amazonas.json';
-import bahia from './bahia.json';
-import ceara from './ceara.json';
-import distritoFederal from './distritoFederal.json';
-import espiritoSanto from './espiritoSanto.json';
-import goias from './goias.json';
-import matoGrossoDoSul from './matoGrossoDoSul.json';
-import minasGerais from './minasGerais.json';
-import para from './para.json';
-import paraiba from './paraiba.json';
-import parana from './parana.json';
-import pernambuco from './pernambuco.json';
-import piaui from './piaui.json';
-import rioDeJaneiro from './rioDeJaneiro.json';
-import rioGrandedoNorte from './rioGrandedoNorte.json';
-import rioGrandeDoSul from './rioGrandeDoSul.json';
-import rondonia from './rondonia.json';
-import roraima from './roraima.json';
-import santaCatarina from './santaCatarina.json';
-import saoPaulo from './saoPaulo.json';
-import sergipe from './sergipe.json';
-import tocantins from './tocantins.json';
-import matoGrosso from './matoGrosso.json';
-import maranhao from './maranhao.json';
+import AC from './AC.json';
+import AM from './AM.json';
+import AP from './AP.json';
+import PA from './PA.json';
+import RO from './RO.json';
+import RR from './RR.json';
+import TO from './TO.json';
+import AL from './AL.json';
+import BA from './BA.json';
+import CE from './CE.json';
+import MA from './MA.json';
+import PB from './PB.json';
+import PE from './PE.json';
+import PI from './PI.json';
+import RN from './RN.json';
+import SE from './SE.json';
+import ES from './ES.json';
+import MG from './MG.json';
+import RJ from './RJ.json';
+import SP from './SP.json';
+import PR from './PR.json';
+import RS from './RS.json';
+import SC from './SC.json';
+import DF from './DF.json';
+import GO from './GO.json';
+import MT from './MT.json';
+import MS from './MS.json';
 
 export const GeoJsonMap = {
-  AC: acre,
-  AL: alagoas,
-  AP: amapa,
-  AM: amazonas,
-  BA: bahia,
-  CE: ceara,
-  DF: distritoFederal,
-  ES: espiritoSanto,
-  GO: goias,
-  MS: matoGrossoDoSul,
-  MG: minasGerais,
-  PA: para,
-  PB: paraiba,
-  PR: parana,
-  PE: pernambuco,
-  PI: piaui,
-  RJ: rioDeJaneiro,
-  RN: rioGrandedoNorte,
-  RS: rioGrandeDoSul,
-  RO: rondonia,
-  RR: roraima,
-  SC: santaCatarina,
-  SP: saoPaulo,
-  SE: sergipe,
-  TO: tocantins,
-  MT: matoGrosso,
-  MA: maranhao,
+  AC,
+  AM,
+  AP,
+  PA,
+  RO,
+  RR,
+  TO,
+  AL,
+  BA,
+  CE,
+  MA,
+  PB,
+  PE,
+  PI,
+  RN,
+  SE,
+  ES,
+  MG,
+  RJ,
+  SP,
+  PR,
+  RS,
+  SC,
+  DF,
+  GO,
+  MT,
+  MS,
 };
