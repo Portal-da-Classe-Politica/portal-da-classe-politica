2737d0a4b64a8fd09abdee71f611ca6884c7894a
Lucas Fabre <lucas.p.fabre@gmail.com>
2024-07-14 17:23:49 +0100

    refac: Radio

--- src/components/base/forms/Radio.tsx
+++ src/components/base/forms/Radio.tsx
@@ -1,3 +1,5 @@
+import { Text } from '@base/text';
+
 export const Radio = ({
   name,
   id = '',
@@ -12,9 +14,12 @@ export const Radio = ({
   className?: string;
 }) => {
   return (
-    <div className={`flex items-center ${className}`}>
-      <input type="radio" name={name} id={id} value={value} className="mr-2" />
-      <label htmlFor={id}>{label}</label>
-    </div>
+    <label className={`flex items-center cursor-pointer ${className}`}>
+      <input type="radio" className="hidden peer" name={name} id={id} value={value} />
+      <div className="w-4 h-4 border-[1px] border-orange rounded-full flex items-center justify-center mr-2 peer-checked:bg-orange"></div>
+      <Text size="B2" textType="span" className="text-grayLight3">
+        {label}
+      </Text>
+    </label>
   );
 };
