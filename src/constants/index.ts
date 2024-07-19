865513710f654ce2ca06a174c879eb754ee951ec
Lucas Fabre <lucas.p.fabre@gmail.com>
2024-07-19 19:19:13 +0100

    chore: update footer texts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,6 +11,13 @@ export const Constants = {
       'https://docs.google.com/document/d/1xnkVr0zzVOcoM_dpdcg3sFIaMELNvzFuyFVOIjiCgb8/edit?usp=sharing',
     userTerms:
       'https://docs.google.com/document/d/16DFSKoLyxKJXZHh2pPFIyg2nkEGtVGcgR-qWUFrXYzA/edit?usp=sharing',
+    instagram: 'https://www.instagram.com/redem.inct',
+    facebook: 'https://www.facebook.com/people/INCT-ReDem/61552965411509',
+    linkedin:
+      'https://www.linkedin.com/company/inct-redem-representa%C3%A7%C3%A3o-e-legitimidade-democr%C3%A1tica/?trk=ppro_cprof&originalSubdomain=br',
+    xTwitter: 'https://x.com/INCT_ReDem',
+    youtube: 'https://www.youtube.com/@INCTReDem',
+    arwPoliticalAdvisory: 'https://www.arwpoliticaladvisory.com',
   },
   loremTitle: 'Consectetur adipiscing elit Suspendisse non odio',
   lorem:
