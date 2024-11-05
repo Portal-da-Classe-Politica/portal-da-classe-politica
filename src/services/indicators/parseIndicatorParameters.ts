33450cfce258fe1afb4a8c8cde048e187a24bc8e
Lucas Fabre <lucas.p.fabre@gmail.com>
2024-11-05 19:23:55 +0000

    fix: topic 4 2024-11-05

--- src/services/indicators/parseIndicatorParameters.ts
+++ src/services/indicators/parseIndicatorParameters.ts
@@ -1,10 +1,14 @@
-import { getParamAsArray } from '@utils/getParamAsArray';
+const BRAZIL = '28';
 
 export const parseIndicatorParameters = (params: URLSearchParams) => {
+  const electoralUnit = params.get('electoralUnit');
+  const uf = params.get('uf');
+  const unidadesEleitorais = uf === BRAZIL ? [] : [uf, electoralUnit];
+
   return {
     initialYear: Number(params.get('initialYear')),
     finalYear: Number(params.get('finalYear')),
     cargoId: Number(params.get('cargoId')),
-    unidadesEleitorais: getParamAsArray(params, 'unidadesEleitorais'),
+    unidadesEleitorais: unidadesEleitorais.filter(n => Boolean(n)).map(n => Number(n)),
   };
 };
