77f636ac637c6fd9bc7b0e6f5bee9b9da7f7045f
Lucas Fabre <lucas.p.fabre@gmail.com>
2024-11-05 19:44:45 +0000

    fix: topic 4 2024-11-05

--- src/app/api/indicators/static-filters/route.ts
+++ src/app/api/indicators/static-filters/route.ts
@@ -36,13 +36,6 @@ const parseUfs = (ufs: any) => {
       return -1;
     }
 
-    if (a.label < b.label) {
-      return -1;
-    }
-
-    if (a.label > b.label) {
-      return 1;
-    }
     return 0;
   });
 };
