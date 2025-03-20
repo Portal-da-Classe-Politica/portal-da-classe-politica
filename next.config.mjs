3c055295ef71410d688cc255cdda6d13c5573226
Chandee <chandemoraes@hotmail.com>
2025-03-20 14:16:37 -0300

    fix: try new webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,28 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        // Add raw-loader for Markdown files
-        config.module.rules.push({
-          test: /\.md$/,
-          use: 'raw-loader',
-        });
-    
-        return config;
-      },
+  webpack: (config, { isServer }) => {
+    // Add raw-loader for Markdown files
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+
+    // Define aliases with absolute paths
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@base": path.resolve(__dirname, "src/components/base"),
+      "@components": path.resolve(__dirname, "src/components"),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
