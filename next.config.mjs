d59a9f1a88e115bb4a4f3771cde0e85026876cdc
Lucas Fabre <lucas.p.fabre@gmail.com>
2025-03-20 21:44:38 +0000

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,9 @@ const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '15mb',
     },
   },
   webpack: (config, { isServer }) => {
