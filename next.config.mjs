0fb18dfaf81a8f5f88b47850f8cb827a76df86e5
Lucas Fabre <lucas.p.fabre@gmail.com>
2025-03-20 20:36:54 +0000

    chore: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,22 +11,28 @@ const nextConfig = {
     // Add raw-loader for Markdown files
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader",
+      use: 'raw-loader',
     });
 
     // 'ol' external
-    config.externals = [...(config.externals || []), "ol"];
+    config.externals = [...(config.externals || []), 'ol'];
+
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'javascript/auto',
+      use: [{ loader: 'json-loader' }],
+    });
 
     // Define aliases with absolute paths
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@base": path.resolve(__dirname, "src/components/base"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@constants": path.resolve(__dirname,"src/constants"),
-      "@hooks": path.resolve(__dirname,"src/hooks/"),
-      "@routes": path.resolve(__dirname,"src/routes"),
-      "@services": path.resolve(__dirname,"src/services"),
-      "@utils": path.resolve(__dirname,"src/utils")
+      '@base': path.resolve(__dirname, 'src/components/base'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     };
 
     return config;
