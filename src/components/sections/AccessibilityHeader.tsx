eb3453704da2ae9514c58c1ef3be83dab914c144
Mateus Marques <mateusmarquesdc@gmail.com>
2025-08-21 20:34:47 -0300

    feat: ajustando logos no header

--- src/components/sections/AccessibilityHeader.tsx
+++ src/components/sections/AccessibilityHeader.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Image from 'next/image';
 
 import { Container, Text } from '@base';
 
@@ -6,22 +6,37 @@ export const AccessibilityHeader = () => {
   return (
     <header className="border-2 border-black border-opacity-5">
       <Container className={'my-5'}>
-        <ul className="flex">
-          <li>
-            <Link target="_blank" href={'/politicas-de-privacidade'}>
-              <Text size="C2" className="text-grayMix4 mr-4">
-                Políticas de Privacidade
-              </Text>
-            </Link>
-          </li>
-          <li>
-            <Link target="_blank" href={'/termos-de-uso'}>
-              <Text size="C2" className="text-grayMix4">
-                Termos de Uso
-              </Text>
-            </Link>
-          </li>
-        </ul>
+        <div className="flex justify-between items-center">
+          <div className="flex-1">
+            <Text size="C2" className="text-grayMix4">
+              Instituto Nacional de Ciência, Tecnologia e Inovação | Representação e Legitimidade Democrática
+              | ReDem
+            </Text>
+          </div>
+          <div className="flex items-center gap-4 ml-8">
+            <Image
+              src="/img/financiamento/inct.jpg"
+              alt="INCT"
+              width={80}
+              height={40}
+              className="h-10 w-auto"
+            />
+            <Image
+              src="/img/financiamento/cnpq.png"
+              alt="CNPq"
+              width={80}
+              height={40}
+              className="h-10 w-auto"
+            />
+            <Image
+              src="/img/financiamento/araucaria.png"
+              alt="Fundação Araucária"
+              width={80}
+              height={40}
+              className="h-10 w-auto"
+            />
+          </div>
+        </div>
       </Container>
     </header>
   );
