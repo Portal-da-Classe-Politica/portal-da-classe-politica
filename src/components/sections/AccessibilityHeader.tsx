06f02c70e9f4e8048744329629a0d6ea9e3214a8
= <=>
2025-08-21 22:24:19 -0300

    feat: ajustando imagem no header

--- src/components/sections/AccessibilityHeader.tsx
+++ src/components/sections/AccessibilityHeader.tsx
@@ -6,36 +6,20 @@ export const AccessibilityHeader = () => {
   return (
     <header className="border-2 border-black border-opacity-5">
       <Container className={'my-5'}>
-        <div className="flex justify-between items-center">
-          <div className="flex-1">
+        <div className="flex justify-between items-center md:justify-between justify-center gap-10">
+          <div className="flex-1 text-center md:text-left">
             <Text size="C2" className="text-grayMix4">
               Instituto Nacional de Ciência, Tecnologia e Inovação | Representação e Legitimidade Democrática
               | ReDem
             </Text>
           </div>
-          <div className="flex items-center gap-4 ml-8">
-            <Image
-              src="/img/financiamento/inct.jpg"
-              alt="INCT"
-              width={80}
-              height={40}
-              className="h-10 w-auto"
-            />
-            <Image
-              src="/img/financiamento/cnpq.png"
-              alt="CNPq"
-              width={80}
-              height={40}
-              className="h-10 w-auto"
-            />
-            <Image
-              src="/img/financiamento/araucaria.png"
-              alt="Fundação Araucária"
-              width={80}
-              height={40}
-              className="h-10 w-auto"
-            />
-          </div>
+          <Image
+            src="/img/financiamento/financiamento.png"
+            alt="INCT"
+            width={333}
+            height={25}
+            className="h-10 w-auto hidden md:block"
+          />
         </div>
       </Container>
     </header>
